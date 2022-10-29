5d91a1743aee7593cadb00f31d2ea4c9d1e9431c
vishnupriya <vishnupriyayadav10@gmail.com>
2022-10-29 10:48:16 +0530

    added

---
+++ sampleapp/sampleapp/Models/usercontroller.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace sampleapp.Models
+{
+    public class usercontroller
+    {
+        public int Number{ get; set; }
+        public string Name { get; set; }
+        public string password { get; set; }
+
+
+    }
+}
